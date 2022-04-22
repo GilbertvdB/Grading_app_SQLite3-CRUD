e4a6cbe56766c06cee6011077994c23c123e54b2
GilbertvdB <gvanderbiezen@gmail.com>
2022-04-22 18:48:43 +0200

    Moved some codes into a function. Saving before I forget.

--- Menu.py
+++ Menu.py
@@ -1,21 +1,47 @@
-import sqlite3
-import test_db_code
+# here we test and modify the menu's
+menu = ["Cijfer Invoeren",
+        "Tweede optie"]
 
-print("Rapport Systeem")
-print()
-print("Menu")
-print("-" * 8)
-print("1. Cijfer Invoeren")
-print("0. Exit")
+sub_menu = ["Nog een update maken?",
+            "Hoofd Menu"]
 
-choice = input("Maak een keuze: ")
+if __name__ == '__main__':
+    # print(enumerate(menu))
 
-while choice != "0":
-    if choice == "1":
-        print()
-        print("Cijfer Invoeren")
+    # print("Rapport Systeem")
+    # print()
+    # print("Menu")
+    # print("-" * 8)
+    # print("1. Cijfer Invoeren")
+    # print("0. Exit")
+    #
+    # choice = input("Maak een keuze: ")
+
+    while True:
+        print("Hoofd Menu:")
+        for index, optie in enumerate(menu):
+            print(f'{index + 1}: {optie}')
+        print("0: Exit")
         print("-" * 20)
-        naam = input("Naam Scholier: ")
-        print(naam)
-    else:
-        break
+
+        choice = int(input("Kies een optie: "))
+
+        if choice == 1:
+            print()
+            print("Cijfer Invoeren")
+            print("-" * 20)
+            scholier = input("Naam Scholier: ")
+            vak = input("Kies vak: ")
+            cijfer = input("Voer cijfer in:  ")
+            print()
+        elif choice == 0:
+            break
+        #
+        # print("Sub Menu:")
+        # for index, optie in enumerate(sub_menu):
+        #     print(f'{index + 1}: {optie}')
+        # print("0: Exit")
+        # print("-" * 20)
+        #
+        # choice = int(input("Kies een optie: "))
+
--- main.py
+++ main.py
@@ -1,34 +1,81 @@
 import sqlite3
-# from CreateDatabase import info_database
+from Menu import menu, sub_menu
+# from test_db_code import update_db, print_report
 
-db = sqlite3.connect("info_data1.sqlite")
-db.execute("CREATE TABLE IF NOT EXISTS info_data1 (naam TEXT, ne TEXT, en TEXT)")
-cursor = db.cursor()
-# menu
-print("Rapport Systeem")
-print()
-print("Menu")
-print("-" * 8)
-print("1. Cijfer Invoeren")
-print("0. Exit")
+class Classy:
+    def update_db(self):
+        #TODO link the scholieren. Maybe via class.
+        query = f"SELECT EXISTS(SELECT * FROM info_data2 WHERE naam = '{scholier}')"
+        data_check = cursor.execute(query)
+        rows = cursor.fetchone()
 
-choice = input("Maak een keuze: ")
+        for x in rows:
+            if x == 0:
+                # print("No Jan in data. Creating data.")
+                cursor.execute(f"INSERT INTO info_data2 (naam, {vak}) VALUES ('{scholier}', '{cijfer}')")
+            else:
+                cursor.execute(f"UPDATE info_data2 SET {vak} = '{cijfer}' WHERE naam = '{scholier}' ")
+                # print("Jan in data")
 
-while choice != "0":
-    if choice == "1":
+        db.commit()
+        print_report()
+
+
+    def print_report(self):
+        data = cursor.execute("SELECT * FROM info_data2")
+        cursor.execute(f"SELECT * FROM info_data2 WHERE naam = '{scholier}'")
+        row = cursor.fetchone()
+        for column in data.description:
+            print(column[0], end='|')
+
+        print()
+        print(*row)
+
+
+    def input_change(self):
         print()
         print("Cijfer Invoeren")
         print("-" * 20)
         scholier = input("Naam Scholier: ")
         vak = input("Kies vak: ")
         cijfer = input("Voer cijfer in:  ")
+        print()
 
-        cursor.execute(f"INSERT INTO info_data1 (naam, {vak}) VALUES ('{scholier}', '{cijfer}')")
-        cursor.close()
-        db.commit()
-        choice = 0
-    else:
+        update_db()
+        print()
+
+
+db = sqlite3.connect("info_data2.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS info_data2 (naam TEXT, ne TEXT, en TEXT)")
+cursor = db.cursor()
+
+# menu
+while True:
+    print("Hoofd Menu:")
+    for index, optie in enumerate(menu):
+        print(f'{index + 1}: {optie}')
+    print("0: Exit")
+    print("-" * 20)
+
+    choice = int(input("Kies een optie: "))
+
+    if choice == 1:
+        input_change()
+        # print()
+        # print("Cijfer Invoeren")
+        # print("-" * 20)
+        # scholier = input("Naam Scholier: ")
+        # vak = input("Kies vak: ")
+        # cijfer = input("Voer cijfer in:  ")
+        # print()
+        #
+        # update_db()
+        # print()
+#TODO after viariable link. Test if options.
+        input("Press ENTER to continue...")
+        print()
+    elif choice == 0:
         break
 
-
+cursor.close()
 db.close()
