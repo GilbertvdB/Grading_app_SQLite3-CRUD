4cdd567349a176df1bdb580e76dc87e1dd4e8cdc
GilbertvdB <gvanderbiezen@gmail.com>
2022-05-26 02:45:53 +0200

    In this update I've added docstrings and comments for all the functions and where ever it was necessary in the code.

--- Menu.py
+++ Menu.py
@@ -11,29 +11,26 @@ x_menu = ["optie 1",
           "optie 3",
           "optie 4"]
 
+# menu addons for the future targeted at specific users
+user_menu = ["Administrator",
+             "Teacher",
+             "Student"]
+
+admin_menu = ["Vak toevoegen",
+              "Placeholder"]
+
+teacher_menu = ["Cijfer Invoeren",
+                "Rapport"]
+
+student_menu = ["Rapport"]
+
 
 def menu(options: list):
-    print("Menu:")
+    print(f"\033[4m" + "Menu" + "\033[0m")
     for index, optie in enumerate(options):
         print(f'{index + 1}: {optie}')
     print("0: Exit")
     print("-" * 20)
-
-
-# def menus():
-#     print("Hoofd Menu:")
-#     for index, optie in enumerate(menu):
-#         print(f'{index + 1}: {optie}')
-#     print("0: Exit")
-#     print("-" * 20)
-#
-#
-# def submenu():
-#     print("Sub Menu:")
-#     for indx, opties in enumerate(sub_menu):
-#         print(f'{indx + 1}: {opties}')
-#     print("0: Exit")
-#     print("-" * 20)
 
 
 if __name__ == '__main__':
@@ -63,25 +60,34 @@ if __name__ == '__main__':
         elif choice == 0:
             break
 
-#     while True:
-#         menus()
-#
-#         choice = int(input("Kies een optie: "))
-#         print()
-#         if choice == 1:
-#             while choice == 1:
-#                 # input_change()
-#                 submenu()
-#
-#                 choice = int(input("Kies een sub optie: "))
-#                 print()
-#             if choice == 0:
-#                 break
-#             else:
-#                 pass
-#         elif choice == 2:
-#             pass
-#         elif choice == 3:
-#             pass
-#         elif choice == 0:
-#             break
+    # #  Display menu style for future addons specific to users
+    # while True:
+    #     menu(user_menu)
+    #     choice = int(input("Kies een optie: "))
+    #     print()
+    #     if choice == 1:
+    #         while choice == 1:
+    #             menu(admin_menu)
+    #             choice = int(input("Kies een optie: "))
+    #             print()
+    #         if choice == 0:
+    #             break
+    #         else:
+    #             pass
+    #     elif choice == 2:
+    #         while choice != 0:
+    #             menu(teacher_menu)
+    #             choice = int(input("Kies een optie: "))
+    #             print()
+    #             if choice == 1:
+    #                 print("Cijfer Invoeren")
+    #                 print()
+    #             elif choice == 2:
+    #                 print("Rappport weergeven")
+    #                 print()
+    #         else:
+    #             pass
+    #     elif choice == 3:
+    #         menu(student_menu)
+    #     elif choice == 0:
+    #         break
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ subject can be added and also the option to view a students report card.
 
 Python versie: 3.10.2
 IDE: IntelliJ IDEA Community Edition 2021.3.2
-Last update: 17/05/2021
+Last update: 26/05/2021
 """
 
 import sqlite3  # library to work with sql database
@@ -19,8 +19,10 @@ from Menu import main_menu, sub_menu  # imports the menu lists
 def menu(menu_list: list):
     """ Generates a menu from a list by using the options in `menu_list`
 
-    :param menu_list: Name of the menu or list where options are defined.
+    :param: menu_list: Name of the menu or list where options are defined.
     """
+
+    # Display a menu in a top-down format.
     print(f"\033[4m" + "Menu" + "\033[0m")
     for index, optie in enumerate(menu_list):
         print(f'{index + 1}: {optie}')
@@ -29,11 +31,16 @@ def menu(menu_list: list):
 
 
 def update_grade():
-    # gets the data that needs to be updated
+    """ Updates a students grade by asking for the details such as,
+    name, subject and grade. Afterwards using the update database
+     method to make the updates."""
+
+    # defines global variables
     global scholier
     global vak
     global cijfer
 
+    # prompts the user for the required data
     print()
     print("Cijfer Invoeren")
     print("-" * 20)
@@ -42,39 +49,46 @@ def update_grade():
     cijfer = input("Voer cijfer in:  ")
     print()
 
+    # runs the method to update the database with the data
     update_db()
     print()
 
 
 def update_db():
-    # checks if the data exists in the table.
+    """ Updates a student database by first checking if the student exist
+    in the database. If so updates the data otherwise creates a new entry
+    with the data provided. """
+
+    # checks if the student name exists in the table.
     cursor.execute(f"SELECT EXISTS(SELECT * FROM info_data2 WHERE naam = '{scholier}')")
     rows = cursor.fetchone()
-
-    # action based on True 1 or False 0
     for x in rows:
-        if x == 0:
+        if x == 0:  # if the data doesn't exist yet, create entry.
             cursor.execute(f"INSERT INTO info_data2 (naam, {vak}) VALUES ('{scholier}', '{cijfer}')")
-        else:
+        else:       # if the data already exist, update entry.
             cursor.execute(f"UPDATE info_data2 SET {vak} = '{cijfer}' WHERE naam = '{scholier}' ")
 
-    db.commit()
-    print_report()
+    db.commit()  # confirm the changes in the database.
+    print_report()  # update the user with the changes made.
 
 
 def print_report():
-    # prints a report after updating data
+    """ Display a student's reportcard by getting the grades from
+      the database and printing it a viewable format."""
+
+    # fetch the student's grades from the database
     data = cursor.execute("SELECT * FROM info_data2")
     cursor.execute(f"SELECT * FROM info_data2 WHERE naam = '{scholier}'")
     row = cursor.fetchone()
-    # horizontal layout
+
+    # display a horizontal layout
     # for column in data.description:
     #     print(column[0], end=' | ')
     # print()
     # print(*row)
     # print()
 
-    # vertical layout
+    # display a vertical layout
     x = 0
     y = '\t'
     for column in data.description:
@@ -85,40 +99,60 @@ def print_report():
 
 
 def print_vak():
+    """ Display a list of the known subjects in the database"""
+
+    # fetch data from the database.
     data = cursor.execute("SELECT * FROM info_data2")
-    row = cursor.fetchone()
-    for column in data.description:
-        if column[0] != 'naam':
-            print(column[0], end='|')
+    # row = cursor.fetchone()
+
+    # print the subject for the user.
+    for column in data.description:  # the headers of the database.
+        if column[0] != 'naam':  # skip the first header which is name.
+            print(column[0], end='|')  # print the subjects.
     print()
+# TODO check both functions for duplicates
 
 
 def print_vak_list():
-    global vak_list
-    vak_list = []
+    """ Creates and displays a list of the subjects from the database to be used
+    globally. """
+
+    global vak_list  # defines global variable.
+    vak_list = []   # creates an empty list.
+
+    # fetch data from the database.
     data = cursor.execute("SELECT * FROM info_data2")
-    row = cursor.fetchone()
-    for column in data.description:
-        if column[0] != 'naam':
-            vak_list.append(column[0])
-    print(*vak_list, sep=' | ')
+    # row = cursor.fetchone()
+
+    # prints a list of the subjects.
+    for column in data.description:     # the headers of the database.
+        if column[0] != 'naam':  # skip 'naam' which is the first header.
+            vak_list.append(column[0])  # add the rest to the list.
+    print(*vak_list, sep=' | ')         # print the list.
 
 
 def update_vak():
+    """ Adds a subject to the database. If it already exists lets the
+    user know. If not then it adds the new subject to the database."""
+
+    # print a list of the already known subjects
     print("Huidige vakken")
     print_vak_list()
     print()
-    vak = input("Vak toevoegen of 0 voor Exit: ")
-    while vak != '0':
-        if vak in vak_list:
+
+    # prompts user for the subject name of 0 to exit.
+    new_vak = input("Vak toevoegen of 0 voor Exit: ")
+    while new_vak != '0':
+        if new_vak in vak_list:  # subject already exists.
             print("Vak bestaat al!")
             print()
-        else:
-            cursor.execute(f"ALTER TABLE info_data2 ADD {vak} TEXT")
+            break
+        else:  # subject does not exist yet. Add to database
+            cursor.execute(f"ALTER TABLE info_data2 ADD {new_vak} TEXT")
             print()
-            print(f"Vak: `{vak}` toegevoegd.")
-            db.commit()
-            print_vak_list()
+            print(f"Vak: `{new_vak}` toegevoegd.")
+            db.commit()  # confirm the changes in the database.
+            print_vak_list()  # let the user know update is complete.
     else:
         print()
 
